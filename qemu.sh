4160ea89113da0dc862de677bcb3d558ed20c2c2
Ian Kronquist <iankronquist@gmail.com>
2017-01-28 15:22:39 -0500

    Shorten sleep timeout

--- qemu.sh
+++ qemu.sh
@@ -35,7 +35,7 @@ install() {
 boot() {
 	qemu-system-x86_64 $KVM -hda $BASE_IMAGE  -redir tcp:$LOCAL_SSH_PORT::22 -m $MEMORY  -cpu $CPU $REBOOT -smp $NUM_CPUS $GRAPHICS &
 	echo "Waiting for ubuntu to boot."
-	sleep 10
+	sleep 1
 	ssh -p $LOCAL_SSH_PORT $USER@localhost
 }
 
